689ddc5976c2b4becc2ada3411ec5bef767d9f16
arelnur <arelnur@gmail.com>
2022-10-14 11:16:39 +0400

    JS1-2

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,4 @@
 console.log("My name is Elnur");
+const myBD = prompt ("Введите дату Вашего рождения (пример: 12/31/1999)")
+const totalDays = (new Date().getTime() - new Date(myBD).getTime()) / 1000 / 60 / 60 / 24
+console.log(totalDays);
